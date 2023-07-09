452eeaf36d9bcf67f3feb5bc5129e6052d400835
Otávio <otaviomarcal785@gmail.com>
2023-07-09 12:40:08 -0300

    add data-name

--- src/script/index.js
+++ src/script/index.js
@@ -4,7 +4,12 @@ arrayPersonagem.forEach((personagem) => {
     personagem.addEventListener('mouseenter', () => {
         const personagemSelecionado = document.querySelector('.selecionado');
         personagemSelecionado.classList.remove('selecionado');
-
         personagem.classList.add('selecionado');
+
+        const personagemGrande = document.querySelector('.personagem-grande');
+        const idPersonagem = personagem.attributes.id.value;
+        personagemGrande.src = `./src/imagens/card-${idPersonagem}.png`;
+        const nomePersonagem = document.getElementById('nome-personagem');
+        nomePersonagem.innerText = personagem.getAttribute('data-name');
     })
 })
